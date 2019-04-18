e62a966aabf64893477c7c1f7299e867111ae89c
Shawkat Qzamil <Shawkat>
2019-04-18 08:59:00 +0300

    Updates

---
+++ precache-manifest.782f17d25a5ff0733a3004ab613ba84b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76970b29a5779221bb98",
+    "url": "/voice-commands-demo/static/js/runtime~main.6baf7d76.js"
+  },
+  {
+    "revision": "74b1801a8ef00b1ffc0c",
+    "url": "/voice-commands-demo/static/js/main.71ed3167.chunk.js"
+  },
+  {
+    "revision": "a29c5abdc1334c5f4649",
+    "url": "/voice-commands-demo/static/js/2.95381c78.chunk.js"
+  },
+  {
+    "revision": "74b1801a8ef00b1ffc0c",
+    "url": "/voice-commands-demo/static/css/main.c02eee22.chunk.css"
+  },
+  {
+    "revision": "bee4c4487a1cee6af05e465b61c0aa8e",
+    "url": "/voice-commands-demo/index.html"
+  }
+];
